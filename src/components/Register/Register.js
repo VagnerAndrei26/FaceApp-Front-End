c90ba3430b37e39cffeaebc2e553be366173f662
Aditya <adikpote@gmail.com>
2022-08-10 20:56:38 +0530

    style change

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -54,7 +54,7 @@ class Register extends Component {
 
 	render() {
 		return (
-			<article className="mw5 bg-white br3 pa3 pa4-ns mv3 ba b--black-10 shadow-5 center">
+			<article className="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
 				<main className="pa4 black-80">
 				  <div className="measure">
 				    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
